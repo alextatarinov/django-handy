35ca1111bc3f8ece08538b0d8a6d546771b07895
Alexandr Tatarinov <tatarinov1997@gmail.com>
2020-04-24 11:34:35 +0300

    lower case fields added

--- django_handy/models/fields.py
+++ django_handy/models/fields.py
@@ -3,12 +3,45 @@ try:
 except ImportError:
     ArrayField = None
 
+import unicodedata
+
 from django import forms
 from django.core.validators import MinValueValidator
 from django.db import models
 
 from django_handy.unique import unique_ordered
 from django_handy.validation import MinValueExclusionValidator
+
+
+class LowerFieldDescriptor:
+    def __init__(self, field):
+        self.field = field
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return None
+        return instance.__dict__[self.field.attname]  # noqa: WPS609
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.field.attname] = self.field.to_python(value)  # noqa: WPS609
+
+
+class LowerCaseFieldMixin:
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value:
+            value = unicodedata.normalize('NFKC', value)
+            if hasattr(value, 'casefold'):  # noqa: WPS421
+                value = value.casefold()
+        return value
+
+    def contribute_to_class(self, cls, name, **kwargs):  # noqa: WPS117
+        super().contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.attname, LowerFieldDescriptor(self))
+
+
+class EmailLowerCaseField(LowerCaseFieldMixin, models.EmailField):
+    pass
 
 
 class DefaultDecimalField(models.DecimalField):
