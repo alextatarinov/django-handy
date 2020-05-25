efbdb3ccd2b843e55f1d84c5c0ce0c314b920477
Alexandr Tatarinov <tatarinov1997@gmail.com>
2020-05-25 13:10:33 +0300

    allow BooleanQ import from top-level models module

--- django_handy/models/__init__.py
+++ django_handy/models/__init__.py
@@ -1,3 +1,5 @@
 from .atomic import call_on_commit
+from .expressions import BooleanQ
 from .fields import *
-from .upsert import bulk_upsert, safe_bulk_upsert
+from .upsert import bulk_upsert
+from .upsert import safe_bulk_upsert
