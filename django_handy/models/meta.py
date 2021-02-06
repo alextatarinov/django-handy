9e9aeef5d9bd0d733d12cd7a8a7d78a4f07fccdf
Alexandr Tatarinov <tatarinov1997@gmail.com>
2021-02-06 18:59:54 +0200

    now really remove upsert and fix get_update_fields api

--- django_handy/models/meta.py
+++ django_handy/models/meta.py
@@ -1,19 +1,21 @@
+from typing import Iterable
 from typing import List
 from typing import Type
 
 from django.db import models
 
 
-def get_update_fields(cls: Type[models.Model]) -> List[str]:
-    """Return list of names of model fields that can be updated excluding unique_fields"""
+def get_update_fields(cls: Type[models.Model], exclude: Iterable[str] = None) -> List[str]:
+    exclude = set(exclude) if exclude is not None else {}
 
-    def _should_be_updated(field):
+    def _can_be_updated(field):
         return (
+            field.name not in exclude and
             not (field.many_to_many or field.one_to_many)
             and not field.auto_created
         )
 
     return [
         field.name for field in cls._meta.get_fields()
-        if _should_be_updated(field)
+        if _can_be_updated(field)
     ]
