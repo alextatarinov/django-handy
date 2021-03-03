ebf0f9b3293237878059ec4cd7dde9e45f7a769e
Alexandr Tatarinov <tatarinov1997@gmail.com>
2021-03-03 20:16:43 +0200

    admin as package also breaks test loader

--- django_handy/tests.py
+++ django_handy/tests.py
@@ -6,8 +6,8 @@ from django.test.runner import DiscoverRunner
 
 class FixedTestLoader(unittest.TestLoader):
     def _find_test_path(self, full_path, *args, **kwargs):
-        # Don't look for tests inside models folder - this will cause invalid models import and RuntimeError
-        if full_path.endswith('/models'):
+        # Don't look for tests inside models and admin packages - this will break Django
+        if full_path.endswith('/models') or full_path.endswith('/admin'):
             return None, False
         return super()._find_test_path(full_path, *args, **kwargs)
 
